518ff08a7fd5d5f15ee36ab461a76f27d97eb5cd
GBTC440007UR <GBTC440007UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-03-25 21:27:29 -0400

    Initial commit

---
+++ src/main/java/com/bymolly/springboot_3033/CourseRepository.java
@@ -0,0 +1,6 @@
+package com.bymolly.springboot_3033;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, Long>{
+}
---
+++ src/main/java/com/bymolly/springboot_3033/Springboot3033Application.java
@@ -0,0 +1,13 @@
+package com.bymolly.springboot_3033;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3033Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3033Application.class, args);
+	}
+
+}
